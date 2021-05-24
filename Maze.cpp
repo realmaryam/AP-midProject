1de31dc35687cf690dfe40d9d6589ac471bdfaa7
maryam <maryambarazande7@gmai.com>
2021-05-24 19:48:09 +0430

    maze

--- Maze.cpp
+++ Maze.cpp
@@ -15,7 +15,6 @@
 
 Maze::Vertex::Vertex( int x , int y )
 {
-    std::cout<<"vertex  constructor\n";
     location.first = x;
     location.second = y;
     neighbour = 0;
@@ -23,30 +22,12 @@ Maze::Vertex::Vertex( int x , int y )
     dirdown = 1;
 }
 
-// Maze::Vertex::Vertex( const Vertex& v1)
-// {
-//     for (size_t i {}; i < 4; i++)
-//     {
-//         dirs[i]= v1.dirs[i]
-//     }
-    
-// }
-
 Maze::Maze(int m , int n)
     : MazeWidth { m }
     , MazeHeight { n }
 {
-    std::cout<<"maze constructor\n";
 }
 
-// int Maze::IsInBounds( int x, int y )
-//     {
-//         if (x < 0 || x >= MazeWidth)
-//             return false;
-//         if (y < 0 || y >= MazeHeight)
-//             return false;
-//         return true;
-//     }
 
 bool Maze::IsOutput( Vertex v )
 {
@@ -79,38 +60,42 @@ void Maze::visit( Vertex& v )
 
 void Maze::makeMaze ()
 {
-    std::cout<<"here is Make maze\n";
     for(int j{0}; j< MazeHeight ; j++)
        { 
            V.push_back(std::vector<Vertex>());
            for(int i{0}; i< MazeWidth ; i++)
             {   
                 Vertex* p  = (new Vertex {i ,j});
-                std::cout<<"here is Make maze\n";
                 V[j].push_back( *p );
-
-                if( p->location.first == MazeWidth-1)
-                   p->dirright = 0; 
-
-                if( p->location.second == MazeHeight-1)
-                   p->dirdown = 0;   
-                std::cout<<p->dirdown<<p->dirright<<std::endl;  
             }
        }
+    for(int j{0}; j< MazeHeight ; j++)
+        for(int i{0}; i< MazeWidth ; i++)
+        {
+            if( V[j][i].location.first == MazeWidth-1)
+                    V[j][i].dirright = 0;
+            if( V[j][i].location.second == MazeHeight-1)
+                    V[j][i].dirdown = 0;
+        }
+
     srand( time(0) );
     int r{};
     for(int j{0}; j< MazeHeight ; j++)
         for(int i{0}; i< MazeWidth ; i++)
         {
-            std::cout<<V[j][i].dirdown<<V[j][i].dirright<<std::endl; 
-            //V[j][i].status =1;
             if(V[j][i].dirdown == 0 )
+            {
                 V[j][i].neighbour = EAST;
+            }
             if(V[j][i].dirright == 0)
+            {
                 V[j][i].neighbour = SOUTH;
+            }
             if(V[j][i].dirdown == 0 && V[j][i].dirright == 0 )
+            {
                 V[j][i].neighbour = NONE ;
-            else
+            }
+            else if (V[j][i].dirdown == 1 && V[j][i].dirright == 1 )
             {
                 r = rand() %2;
                 if(r == 0 )
@@ -119,28 +104,19 @@ void Maze::makeMaze ()
                     V[j][i].neighbour = SOUTH;  
             }  
         }
-    //std::cout<<V[MazeHeight-1][MazeWidth-1].neighbour<<std::endl;
-    //show(V);
+
+    show(V);
 }
 
 
-// void Maze::check( std::vector<std::vector<Vertex>> V , int x , int y)
-// {
-//     V[y][x].status = 1;
-//     if ( V[y][x].neighbour == EAST && V[y][x].dirright && !V[y][x+1].status)
-//                 {
-//                     grid[ XYToIndex(x , y) ] = ' ';
-//                     check ( V , x+1 , y );
-//                 }
-//     if ( V[y][x].neighbour == SOUTH && V[y][x].dirdown && !V[y+1][x].status)
-//                 {
-//                     grid[ XYToIndex(x , y) ] = ' ';
-//                     check ( V , x , y+1 );
-//                 }
-// }
-
 void Maze::show( std::vector<std::vector<Vertex>> V )
 {
+    for (int i { } ; i < 2 * MazeWidth + 1 ; i++)
+    {
+        std::cout<<"#";
+    }
+    std::cout<<std::endl;
+
     for(int j{}; j< 2 * MazeHeight ; j++)
     {
         grid.push_back(std::vector<char>());
@@ -164,6 +140,8 @@ void Maze::show( std::vector<std::vector<Vertex>> V )
             }
         }
     }
+    grid[0][0] = ' ';
+    grid[MazeHeight][MazeWidth] = ' ';
     for(int j{}; j< 2* MazeHeight ; j++)
     {
         std::cout<<"#";
